f38d36170405e5f4eadd8cb96cc0e04d82824cf4
af-coronel <alvaro.coronel@mi.unc.edu.ar>
2026-09-13 20:41:41 -0300

    feat: Función para inicializar la base de datos

--- server.py
+++ server.py
@@ -2,3 +2,17 @@
 import socket
 import sqlite3
 
+# Función para inicializar la base de datos
+def init_db():
+    conn = sqlite3.connect("chat.db")
+    cursor = conn.cursor()
+    cursor.execute("""
+     CREATE TABLE IF NOT EXISTS mensajes (
+       id INTEGER PRIMARY KEY AUTOINCREMENT,
+       contenido TEXT NOT NULL,
+       fecha_envio DATETIME DEFAULT CURRENT_TIMESTAMP,
+       ip_cliente TEXT NOT NULL
+      )
+    """)
+    conn.commit()
+    conn.close()
